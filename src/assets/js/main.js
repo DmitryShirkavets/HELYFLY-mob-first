457074cf2e6ba10417ac05c37df3a5ff8d3055b0
DmitryShirkavets <46999066+DmitryShirkavets@users.noreply.github.com>
2021-09-04 11:29:18 +0300

    rename media

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,11 +4,17 @@
 
 let heightGallery = document.querySelector('.gallery__list');
 const buttonGallery = document.querySelector('.gallery__button');
+const buttonGalleryBefore = document.querySelector('.gallery__button::before');
+const buttonGalleryAfter = document.querySelector('.gallery__button::after');
 
 buttonGallery.onclick = function() {
     if(heightGallery.style.height == 'auto'){
         heightGallery.style.height = '385px';
+        buttonGalleryBefore.style.transform = 'rotate(0deg)';
+        buttonGalleryAfter.style.transform = 'rotate(0deg)';
     }else{
         heightGallery.style.height = 'auto';
+        buttonGalleryBefore.style.transform = 'rotate(180deg)';
+        buttonGalleryAfter.style.transform = 'rotate(180deg)';
     }
 }
