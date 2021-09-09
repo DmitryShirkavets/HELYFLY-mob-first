74a8bb4a4710f2f98fcae7b9f4e11e804c27a17c
DmitryShirkavets <46999066+DmitryShirkavets@users.noreply.github.com>
2021-09-09 22:04:11 +0300

    update main.js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,20 +1,13 @@
+// GALLERY LIST
 
-
-// GALLERY
-
-let heightGallery = document.querySelector('.gallery__list');
 const buttonGallery = document.querySelector('.gallery__button');
-const buttonGalleryBefore = document.querySelector('.gallery__button::before');
-const buttonGalleryAfter = document.querySelector('.gallery__button::after');
+const listGallery = document.querySelector('.gallery__list');
 
 buttonGallery.onclick = function() {
-    if(heightGallery.style.height == 'auto'){
-        heightGallery.style.height = '385px';
-        buttonGalleryBefore.style.transform = 'rotate(0deg)';
-        buttonGalleryAfter.style.transform = 'rotate(0deg)';
+    if(listGallery.classList.contains('active')){
+        listGallery.classList.remove('active');
     }else{
-        heightGallery.style.height = 'auto';
-        buttonGalleryBefore.style.transform = 'rotate(180deg)';
-        buttonGalleryAfter.style.transform = 'rotate(180deg)';
+        listGallery.classList.add('active');
     }
+    
 }
